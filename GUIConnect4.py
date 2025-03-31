0b789ef7675fa9001d0fb65ad2e5286cf2fdfaaa
Lily <gahymes@gmail.com>
2025-03-31 13:33:25 -0500

    added diagonal win condition

--- GUIConnect4.py
+++ GUIConnect4.py
@@ -40,13 +40,15 @@ class GUI:
         self.currentPlayer = 1
 
         #Movement Buttons
-        button0 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(0))
-        button1 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(1))
-        button2 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(2))
-        button3 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(3))
-        button4 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(4))
-        button5 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(5))
-        button6 = Button(self.root, text="Move Here", command = lambda: self.ButtonPress(6))
+        button0 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(0))
+        button1 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(1))
+        button2 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(2))
+        button3 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(3))
+        button4 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(4))
+        button5 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(5))
+        button6 = Button(self.root, text="Move Here", command = lambda: self.MoveButtonPress(6))
+
+        clearButton = Button(self.root, text="Clear Board", command = lambda: self.ClearButtonPress())
         
         # Movement Button Placement
         button0.grid(row = 0, column = 0)
@@ -57,6 +59,8 @@ class GUI:
         button5.grid(row = 0, column = 5)
         button6.grid(row = 0, column = 6)
 
+        clearButton.grid(row = 0, column = 7)
+
         self.displayBoard()
 
         self.root.mainloop()
@@ -64,7 +68,7 @@ class GUI:
     def displayBoard(self):
         None
 
-        canvas = Canvas(self.root, width=(95*self.x_dimension), height = (95*self.y_dimension))
+        canvas = Canvas(self.root, width=(100*self.x_dimension), height = (100*self.y_dimension))
         canvas.grid(row = 1, column = 0, columnspan = self.x_dimension, rowspan=self.y_dimension, sticky=NW)
 
         for columnNum, column in enumerate(self.board):
@@ -72,15 +76,15 @@ class GUI:
             for rowNum, entry in enumerate(column):
 
                 if entry == 0:
-                    canvas.create_rectangle(columnNum * 95, rowNum * 95, (columnNum+1)*95, (rowNum+1)*95, fill="white")
+                    canvas.create_rectangle(columnNum * 100, rowNum * 100, (columnNum+1)*100, (rowNum+1)*100, fill="white")
                 elif entry == 1:
-                    canvas.create_rectangle(columnNum * 95, rowNum * 95, (columnNum+1)*95, (rowNum+1)*95, fill="red")
+                    canvas.create_rectangle(columnNum * 100, rowNum * 100, (columnNum+1)*100, (rowNum+1)*100, fill="red")
                 elif entry == 2:
-                    canvas.create_rectangle(columnNum * 95, rowNum * 95, (columnNum+1)*95, (rowNum+1)*95, fill="blue")
+                    canvas.create_rectangle(columnNum * 100, rowNum * 100, (columnNum+1)*100, (rowNum+1)*100, fill="blue")
 
         canvas.create_rectangle
 
-    def ButtonPress(self, slot):
+    def MoveButtonPress(self, slot):
         
         self.board[slot][self.topRow[slot]] = self.currentPlayer
 
@@ -131,8 +135,64 @@ class GUI:
             print(f"Player {player} Wins!")
             return 1 
 
-        #Check Diagonal
+        #Check Diagonal Left Up Right Down
+        inRow = 1
+        for num in range(1, self.connectNum): # Left Up
+            if (movex - num >= 0 and movey - num >= 0):
+                if (self.board[movex-num][movey-num] == player):
+                    inRow += 1
+                else:
+                    break
+
+        for num in range(1, self.connectNum): # Right Down
+            if (movex + num < self.x_dimension and movey + num < self.y_dimension):
+                if (self.board[movex+num][movey+num] == player):
+                    inRow += 1
+                else:
+                    break
+
+        if (inRow >= self.connectNum):
+            print(f"Player {player} Wins!")
+            return 1
+
+        #Check Diagonal Right Up Left Down
+        inRow = 1
+        for num in range(1, self.connectNum): # Right Up
+            if (movex + num < self.x_dimension and movey - num >= 0):
+                if (self.board[movex+num][movey-num] == player):
+                    inRow += 1
+                else:
+                    break
+
+        for num in range(1, self.connectNum): # Left Down
+            if (movex - num >= 0 and movey + num < self.y_dimension):
+                if (self.board[movex-num][movey+num] == player):
+                    inRow += 1
+                else:
+                    break
+
+        if (inRow >= self.connectNum):
+            print(f"Player {player} Wins!")
+            return 1                         
+        
+
+    def ClearButtonPress(self):
     
+        print("clearing")
+
+        self.board = []
+        self.topRow = []
+
+        self.currentPlayer = 1
+
+        for i in range(self.x_dimension):
+            junk = []
+            self.topRow.append(self.y_dimension-1)
+            for j in range (self.y_dimension):
+                junk.append(0)
+            self.board.append(junk)
+
+        self.displayBoard()
 
 if __name__ == "__main__":
     GUI()
