b9b5c1ab173252917c1a5bc31bc338eeeb4a7d9e
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-05-08 17:51:47 -0500

    mtcs add

--- GUIConnect4.py
+++ GUIConnect4.py
@@ -3,6 +3,8 @@ import argparse
 import numpy as np
 import randomMove
 import Minimax
+import ABPruning
+import MCTS
 import time
 
 class Move:
@@ -148,6 +150,8 @@ class GUI:
         start_time = time.time()
         # slot = randomMove.randomMove(self.board, self.topRow, self.availableMoves, self.currentPlayer)
         slot = Minimax.minimax_move(self.board, self.topRow, self.availableMoves, self.currentPlayer, 6)
+        mcts = MCTS(self.board, self.topRow, self.availableMoves, self.currentPlayer, depth=6)
+        slot = mcts.mcts_move()
         #print(f"AI Operation Took: {time.time() - start_time} seconds")
         self.timing[self.currentPlayer] += (time.time() - start_time)
 
